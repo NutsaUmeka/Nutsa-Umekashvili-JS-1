8f0f97a4cd762e28dbaf219fa692b55c8faef866
NutsaUmeka <nuciacule@gmail.com>
2024-11-02 06:32:49 +0400

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+let Name = "Jon";
+let Surname = "Snow";
+let Age = 21;
+let Email = "kingofthenorth@gmail.com";
+let PhoneNumb = "551 75 05 85";
+let Underage = false;
+let Adult = true;
+
+console.log("His name is" , Name , Surname , ", He is" , Age , "years old ," , "His email is" , Email , 
+    ", His phone number is" , PhoneNumb);
+
+console.log(typeof Age);
+console.log(typeof Adult);
+console.log(typeof Underage);
+console.log("%c false" , "color:yellow");
+console.log("%c true" , "color:yellow");
+console.log("%c 551 75 05 85" , "color:red");
